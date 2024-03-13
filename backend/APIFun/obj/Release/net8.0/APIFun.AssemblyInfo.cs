3d625f2dbef35d690e0c409847ac7fb45cecb240
haileybronson <haileyp7@byu.edu>
2024-03-13 15:22:38 -0600

    updates?

--- backend/APIFun/obj/Release/net8.0/APIFun.AssemblyInfo.cs
+++ backend/APIFun/obj/Release/net8.0/APIFun.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIFun")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+552b90658e0b70a83f5bc9eadb6e3f39e664042f")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIFun")]

3a98ae9502733e56943cd4f68d80cad892171b9f
haileybronson <haileyp7@byu.edu>
2024-03-13 15:20:21 -0600

    trash

--- backend/APIFun/obj/Release/net8.0/APIFun.AssemblyInfo.cs
+++ backend/APIFun/obj/Release/net8.0/APIFun.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIFun")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a094d07e1130b7fd360bb71ec9b282bb142c427")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIFun")]
